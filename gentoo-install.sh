4bd6582f0c44bee629184d34175a83aea215e875
PauloGabrielLeme <132236197+PauloGabrielLeme@users.noreply.github.com>
2025-10-22 16:25:25 -0300

    Update gentoo-install.sh

--- gentoo-install.sh
+++ gentoo-install.sh
@@ -45,3 +45,7 @@ emerge gnome-base/gnome-light dev-lang/rust app-editors/helix x11-terms/xfce4-te
 emerge app-shells/fish gnome-extra/gnome-shell-extension-pop-shell
 emerge -aC gnome-extra/gnome-shell-extension-pop-shell
 emerge sys-apps/flatpak net-im/discord media-sound/spotify
+emerge media-fonts/noto media-fonts/noto-emoji media-fonts/noto-cjk
+
+exit
+
